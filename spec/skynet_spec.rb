225edce251d24e704edb8b8393359783e0f9fe59
Fahad Ahmed <fahad838@gmail.com>
2024-12-27 09:36:53 +0530

    Add doubles and stub to the spec file

--- spec/skynet_spec.rb
+++ spec/skynet_spec.rb
@@ -1,16 +1,17 @@
 require 'skynet'
 
 describe Skynet do
+  let(:terminator) { double }
   
   it 'inspects terminator' do
-    terminator = Terminator.new
     expect(subject.inspection_one(terminator)).to eq terminator
   end
 
   it 'inspects terminators and test modes are set correctly' do
-    terminator = Terminator.new
     message = "Inspection Passed"
+    allow(terminator).to receive(:kill_test) { false }
+    allow(terminator).to receive(:learn_test) { message }
     expect(subject.inspection_two(terminator)).to eq message
   end
-  
+
 end
